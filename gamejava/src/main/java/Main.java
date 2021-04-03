2b8246ec72e805559fb83e9405082622cd5de0f8
mariomui <yi.kan.mui@gmail.com>
2021-04-03 01:52:12 -0700

    overwrite old stuff

---
+++ gamejava/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        //        try {
+
+        Window window;
+        window = Window.get();
+        window.run();
+
+
+    }
+}
